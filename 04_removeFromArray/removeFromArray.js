8f2d24a8ddaf1c04b79cf178eba0c54e65c9e220
Hiatt Zhao <hiattzhao@gmail.com>
2024-06-17 19:03:51 -0400

    Complete exercises 4 to 7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elements) {
+
+    for (let i = 0; i < elements.length; i++) {
+        while(array.includes(elements[i])) {
+            let index = array.indexOf(elements[i]);
+            if (index >= 0) {
+                array.splice(index, 1);
+            }
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
+    if (start > end) {
+        let temp = end;
+        end = start;
+        start = temp;
+    }
+    
+    if (start < 0 || end < 0) {
+        return 'ERROR';
+    }
+    
+    if (typeof start !== "number" || typeof end !== "number") {
+        return "ERROR";
+    }
 
+    for (let i = start; i <= end ; i++) {
+        sum += i;
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
