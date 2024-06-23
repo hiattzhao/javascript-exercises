6639882ea672f6b0825106699e1fc227dc23b445
Hiatt Zhao <hiattzhao@gmail.com>
2024-06-23 13:40:44 -0400

    Complete exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    // Solution without regex:
+    // const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+    // const cleanString = string.toLowerCase()
+    //                         .split("")
+    //                         .filter(letter => alphanumerical.includes(letter))
+    //                         .join("");
+    // const reversedString = cleanString.split("").reverse().join("");
+    // return cleanString === reversedString;
+
+    // Below code also works:
+    let newString = string.toLowerCase().replace(/[^\w\s\']|_/g, "")
+         .replace(/\s+/g, "");
+    return (newString === newString.split("").reverse().join(""));
 };
 
 // Do not edit below this line
