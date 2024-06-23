7b59462de79b7ac9bda1a84364753b435095b527
Hiatt Zhao <hiattzhao@gmail.com>
2024-06-23 14:43:49 -0400

    Complete exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
+    // Calculate age
+    function calcAge(birth, death) {
+        death ??= new Date().getFullYear();
+        return death - birth;
+    }
 
+    let oldestToYoungestArray =  peopleArray.sort((a, b) => calcAge(b.yearOfBirth, b.yearOfDeath) - calcAge(a.yearOfBirth, a.yearOfDeath))
+    return oldestToYoungestArray[0];
 };
 
 // Do not edit below this line
